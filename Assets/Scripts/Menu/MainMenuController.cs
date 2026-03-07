5910e55f616c944abb5f7384467b9262218dd7b8
yort098 <trham098@gmail.com>
2026-03-07 00:36:33 -0500

    Added quit button and a little more styling

--- Assets/Scripts/Menu/MainMenuController.cs
+++ Assets/Scripts/Menu/MainMenuController.cs
@@ -1,4 +1,5 @@
 using Cysharp.Threading.Tasks;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -10,25 +11,63 @@ public class MainMenuController : MonoBehaviour
 
     private Button _startBttn;
     private Button _optionsBttn;
+    private Button _quitBttn;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        _root = GetComponent<UIDocument>().rootVisualElement;
+        _root = GetComponent<UIDocument>().rootVisualElement;      
+    }
 
+    private void OnEnable()
+    {
         _startBttn = _root.Query<Button>("Start");
-        _optionsBttn = _root.Query<Button>("Options");
+        _startBttn.clicked += OnStartButtonClicked;
+        _startBttn.RegisterCallback<MouseEnterEvent>(ButtonBehavior.OnMouseEnter);
+        _startBttn.RegisterCallback<MouseLeaveEvent>(ButtonBehavior.OnMouseExit);
 
-        _startBttn.RegisterCallback<ClickEvent>(OnStartGame);
+        _optionsBttn = _root.Query<Button>("Options");
+        _optionsBttn.clicked += OnOptionsButtonClicked;
+        _optionsBttn.RegisterCallback<MouseEnterEvent>(ButtonBehavior.OnMouseEnter);
+        _optionsBttn.RegisterCallback<MouseLeaveEvent>(ButtonBehavior.OnMouseExit);
+
+        _quitBttn = _root.Query<Button>("Quit");
+        _quitBttn.clicked += OnQuitButtonClicked;
+        _quitBttn.RegisterCallback<MouseEnterEvent>(ButtonBehavior.OnMouseEnter);
+        _quitBttn.RegisterCallback<MouseLeaveEvent>(ButtonBehavior.OnMouseExit);
     }
 
     private void OnDisable()
     {
-        _startBttn.UnregisterCallback<ClickEvent>(OnStartGame);
+        _startBttn.clicked -= OnStartButtonClicked;
+        _startBttn.UnregisterCallback<MouseEnterEvent>(ButtonBehavior.OnMouseEnter);
+        _startBttn.UnregisterCallback<MouseLeaveEvent>(ButtonBehavior.OnMouseExit);
+
+        _optionsBttn.clicked -= OnOptionsButtonClicked;
+        _optionsBttn.UnregisterCallback<MouseEnterEvent>(ButtonBehavior.OnMouseEnter);
+        _optionsBttn.UnregisterCallback<MouseLeaveEvent>(ButtonBehavior.OnMouseExit);
+
+        _quitBttn.clicked -= OnQuitButtonClicked;
+        _quitBttn.RegisterCallback<MouseEnterEvent>(ButtonBehavior.OnMouseEnter);
+        _quitBttn.RegisterCallback<MouseLeaveEvent>(ButtonBehavior.OnMouseExit);
     }
 
-    private void OnStartGame(ClickEvent evt)
+    private void OnStartButtonClicked()
     {
         _sceneController.ChangeScene(Scene.Inventory);
+    }
+
+    private void OnOptionsButtonClicked()
+    {
+        Debug.Log("There are no options yet...");
+    }
+
+    private void OnQuitButtonClicked()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
     }
 }
